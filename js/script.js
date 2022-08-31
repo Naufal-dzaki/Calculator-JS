df0e42cb63dfe0ca0453a50fc0cad81a215957cb
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2022-08-31 14:12:29 +0700

    change font color to red for empty input warning

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ function hasil(){
         document.getElementById("textview").innerHTML = eval(nilai);
     }else if(nilai == "" ){
         document.getElementById("textview").innerHTML = "empty!";
+        document.getElementById("textview").classList = "col-12 border-0 custom-textview d-flex py-4 text-danger fw-bold fs-4";
         setTimeout(() => (document.getElementById("textview").innerHTML = ""), 2000);
+        setTimeout(() => (document.getElementById("textview").classList = "col-12 border-0 custom-textview d-flex py-4 text-white-50 fw-bold fs-4"), 2000);
     }
 }
 
